7b22e19d4803984f2f5126d29cd8367bbfb17f49
Dmitry Vasilyev <vasilyevdimitry001@mail.ru>
2022-02-17 20:36:54 +0500

    deleted unused libs

--- src/components/main/components/officers.js
+++ src/components/main/components/officers.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { delOfficer } from '../../../store/actions';
